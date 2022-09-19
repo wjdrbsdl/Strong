853a5d54f254174e92f71cc50b6694d97c555cc5
Bbyu <wjdrbsdl1011@naver.com>
2022-09-19 13:27:41 +0900

    [feat] 생성한 맵 블록을 배열 순대로 정렬

--- Assets/Script/MakeMap.cs
+++ Assets/Script/MakeMap.cs
@@ -12,12 +12,14 @@ public class MakeMap : MonoBehaviour
     const int MAP_WIDTH_SIZE = 5;
     private MapType[,] m_mapNum = null;
     private int m_mapTypeCount;
+    [SerializeField] private GameObject m_mapBlock; 
 
     // Start is called before the first frame update
     void Start()
     {
         m_mapTypeCount = System.Enum.GetValues(typeof(MapType)).Length;
         MakeBaseLand();
+        ReplaceBaseLand();
     }
 
     private void MakeBaseLand()
@@ -29,8 +31,23 @@ public class MakeMap : MonoBehaviour
             {
                 int mapType = Random.Range(0, m_mapTypeCount);
                 m_mapNum[length, width] = (MapType)mapType;
-                Debug.Log("지도 " + width + " " + length + "위치에 지형 블록 생성 해당 블록의 속성은 "+(MapType)mapType);
+                Debug.Log("지도 " + length + " " + width + "위치에 지형 블록 생성 해당 블록의 속성은 "+(MapType)mapType);
 
+            }
+        }
+    }
+
+    private void ReplaceBaseLand()
+    {
+        float xScale = m_mapBlock.transform.localScale.x;
+        float yScale = m_mapBlock.transform.localScale.y;
+        for (int length = 0; length < MAP_LENGTH_SIZE; length++)
+        {
+            for (int width = 0; width < MAP_WIDTH_SIZE; width++)
+            {
+                GameObject mapBlock = Instantiate(m_mapBlock);
+                Vector2 place = new Vector2(width * xScale, length * yScale);
+                mapBlock.transform.position = place;
             }
         }
     }
