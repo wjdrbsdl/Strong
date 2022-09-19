fd50f176851bc1a04810a3ad793b70d1b7339931
Bbyu <wjdrbsdl1011@naver.com>
2022-09-19 18:30:13 +0900

    줄 정리

--- Assets/Script/MakeMapBlock.cs
+++ Assets/Script/MakeMapBlock.cs
@@ -25,7 +25,6 @@ public class MakeMapBlock : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      
         MakeBlock();
         ReplaceBlock();
         OrderExit();
